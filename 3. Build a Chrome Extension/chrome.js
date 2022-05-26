08aa59d60da06d2b5bdcc37e387de72a11451dce
JSShant <joshua.shanthikumar@outlook.com>
2022-05-26 23:34:11 +1000

    Use render function to push input to innerHTML UL

--- 3. Build a Chrome Extension/chrome.js
+++ 3. Build a Chrome Extension/chrome.js
@@ -2,18 +2,28 @@
 //     console.log("Button Clicked from onclick")
 // }
 
-let myLeads = ["yes","no","maybe"]
+let myLeads = []
 const inputEl = document.getElementById("input__el")
 const inputBtn = document.getElementById("input__btn")
 const ulEl = document.getElementById("ul__el")
-
+let listItems = ""
 inputBtn.addEventListener("click", function() {
     console.log("Button Clicked from event listener!")
     //myLeads.push("www.awesomelead.com") - this is a hardcoded value
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    renderLeads()
 })
 //Const cannot be reassigned
-for (let i = 0; i < myLeads.length; i += 1) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+    for (let i = 0; i < myLeads.length; i += 1) {
+        //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li) (
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
 }
+
+//creating list item variable /Add array to Listitems instead of ulEL.innerHTML/Render listItems inside ulEl.innerHTML 
