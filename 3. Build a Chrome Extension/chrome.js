29273809dab9e1659870f8f020e3c3fa1af0c1b6
JSShant <joshua.shanthikumar@outlook.com>
2022-05-26 22:40:31 +1000

    Pushing value entered in input field

--- 3. Build a Chrome Extension/chrome.js
+++ 3. Build a Chrome Extension/chrome.js
@@ -1,8 +1,16 @@
-function save() {
-    console.log("Button Clicked from onclick")
-}
+// function save() {
+//     console.log("Button Clicked from onclick")
+// }
 
-let inputBtn = document.getElementById("input__btn")
+let myLeads = []
+const inputEl = document.getElementById("input__el")
+const inputBtn = document.getElementById("input__btn")
+
 inputBtn.addEventListener("click", function() {
     console.log("Button Clicked from event listener!")
+    myLeads.push("www.awesomelead.com")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
+//Const cannot be reassigned
+
