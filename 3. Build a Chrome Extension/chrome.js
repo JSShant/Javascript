9ecbaa7ec57b92b62eaad7467806917b552a5a6d
JSShant <joshua.shanthikumar@outlook.com>
2022-05-26 23:01:04 +1000

    Using innerHTML

--- 3. Build a Chrome Extension/chrome.js
+++ 3. Build a Chrome Extension/chrome.js
@@ -2,15 +2,18 @@
 //     console.log("Button Clicked from onclick")
 // }
 
-let myLeads = []
+let myLeads = ["yes","no","maybe"]
 const inputEl = document.getElementById("input__el")
 const inputBtn = document.getElementById("input__btn")
+const ulEl = document.getElementById("ul__el")
 
 inputBtn.addEventListener("click", function() {
     console.log("Button Clicked from event listener!")
-    myLeads.push("www.awesomelead.com")
+    //myLeads.push("www.awesomelead.com") - this is a hardcoded value
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
 //Const cannot be reassigned
-
+for (let i = 0; i < myLeads.length; i += 1) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
