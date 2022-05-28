23a0394df8c15227f0fa5b9b8c4a6d8a1cfa6cf3
JSShant <joshua.shanthikumar@outlook.com>
2022-05-28 22:07:46 +1000

    Check if it is truthy or falsy

--- 3. Build a Chrome Extension/practice.js
+++ 3. Build a Chrome Extension/practice.js
@@ -2,3 +2,6 @@ const boxBtn = document.getElementById("box__btn")
 boxBtn.addEventListener("click", function() {
     console.log("I want to open the box")
 })
+
+let trueOfFalse = Boolean("hello")
+console.log(trueOfFalse)
