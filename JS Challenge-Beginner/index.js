9f692fa398ae32133afe39bc78e556b5a3fa2eba
JSShant <joshua.shanthikumar@outlook.com>
2022-06-01 23:23:42 +1000

    rectangle perimeter, triangle area, extend strring

--- JS Challenge-Beginner/index.js
+++ JS Challenge-Beginner/index.js
@@ -11,3 +11,26 @@ function seconds(num1) {
     return num1*60*60
 }
 console.log(seconds(2))
+
+//Perimeter of Rectangle
+function permiter(num1, num2) {
+    return 2*(num1 + num2)
+}
+console.log(permiter(6,7))
+
+//Area of Triangle
+function triangleArea(base, height){
+    return 0.5*base*height
+}
+console.log(triangleArea(3,2))
+
+//extending a string
+function string(string) {
+    return string + "frontend"
+}
+console.log(string("apple"))
+
+function stringNumber(string) {
+    return +string + 1 //putting +infront converts to number
+}
+console.log(stringNumber("3"))
