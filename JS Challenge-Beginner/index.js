0687ac90058f0f78eb6239c77e3453ff8a96adaf
JSShant <joshua.shanthikumar@outlook.com>
2022-06-01 23:13:54 +1000

    Addtion/Converting Hours into seconds

---
+++ JS Challenge-Beginner/index.js
@@ -0,0 +1,13 @@
+
+// Sum of two variables
+function addition(num1, num2) {
+    return num1 + num2
+    
+}
+console.log(addition(3,5));
+
+//Converting Hours into seconds
+function seconds(num1) {
+    return num1*60*60
+}
+console.log(seconds(2))
