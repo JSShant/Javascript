1c0ba5b7b01c4751934e00efdf8b116afe27928c
JSShant <joshua.shanthikumar@outlook.com>
2022-06-04 22:19:59 +1000

    Finding largest number in an array

--- Js Challenge- Medium/index.js
+++ Js Challenge- Medium/index.js
@@ -23,7 +23,8 @@ console.log(arrLength([1, 2, 3, 4]))
 function lastElem(arr) {
   return arr[arr.length - 1] //Arr.length gives the last count of array + 1 (because first one is 0)
 }
-console.log(lastElem([1, 2, 3, 6]))
+console.log(lastElem([1, 2, 3, 11]))
+
 
 //find the sum of an array
 function arrSum(arr) {
@@ -66,3 +67,15 @@ function calcTime(seconds) {
   return timerMinutes + ":" + timerSeconds
 }
 console.log(calcTime(70))
+
+//Find the largest number
+function getMax(arr) {
+  let max = arr[0]
+  for (let i=0; i<arr.length; ++i) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+  }
+  return max
+}
+console.log(getMax([100, 200, -300]))
